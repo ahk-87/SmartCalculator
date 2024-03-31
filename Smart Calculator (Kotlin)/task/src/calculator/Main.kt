37a540f5c15f00d9cef8fd8a17a02535475c30ef
ahk-87 <kazak.abed@gmail.com>
2024-03-31 21:25:23 +0300

    Minor changes

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -5,7 +5,7 @@ import kotlin.math.pow
 
 class Calc {
     val variables = mutableMapOf<String, BigInteger>()
-    val operatorsMap = mapOf("+" to 0, "-" to 0, "*" to 1, "/" to 1, "^" to 2)
+    val operatorsMap = mapOf("+" to 1, "-" to 1, "*" to 2, "/" to 2, "^" to 3)
 
     fun execute(str: String) {
         if (str.contains("="))
@@ -35,14 +35,16 @@ class Calc {
     }
 
     private fun equation(str: String) {
-        val numbers = mutableListOf<BigInteger>(BigInteger("0"))
-        val eq = checkEquation(str)
+        val numbers = mutableListOf(BigInteger("0"))
 
-        if (eq.isEmpty()) {
+        val eq = try {
+            tokenizer(str)
+        } catch (e: Exception) {
             println("Invalid expression"); return
         }
 
         val postEquation = infixToPostfix(eq)
+
         while (postEquation.isNotEmpty()) {
             val u = postEquation.removeFirst()
             if (u in operatorsMap.keys) {
@@ -68,8 +70,8 @@ class Calc {
         }
     }
 
-    fun checkEquation(eq: String): List<String> {
-        val el = mutableListOf<String>()
+    fun tokenizer(eq: String): List<String> {
+        val tokens = mutableListOf<String>()
         var parenthesesCount = 0
         var variable = ""
         var operator = "+"
@@ -79,25 +81,25 @@ class Calc {
         for (c in eq.trim()) {
             when {
                 c == '(' -> {
-                    if (variable.isNotEmpty()) return emptyList()
+                    if (variable.isNotEmpty()) throw IllegalArgumentException()
                     clearAll()
-                    parenthesesCount++; el.add(c.toString())
+                    parenthesesCount++; tokens.add(c.toString())
                     operator = c.toString()
                 }
 
                 c == ')' -> {
-                    if (operator.isNotEmpty()) return emptyList()
+                    if (operator.isNotEmpty()) throw IllegalArgumentException()
                     clearAll()
-                    parenthesesCount--; el.add(c.toString())
+                    parenthesesCount--; tokens.add(c.toString())
                 }
 
                 c == '+' -> {
                     if (operator.isEmpty()) {
                         clearAll()
                         operator = c.toString()
-                        el.add(operator)
+                        tokens.add(operator)
                     }
-                    if (operator != "+") return emptyList()
+                    if (operator != "+") throw IllegalArgumentException()
                 }
 
                 c == '-' -> {
@@ -105,28 +107,28 @@ class Calc {
                         clearAll()
                         operator = c.toString()
                     } else if (operator == "-") {
-                        operator = "+"; el.removeLastOrNull()
+                        operator = "+"; tokens.removeLastOrNull()
                     } else if (operator == "+") {
-                        operator = "-"; el.removeLastOrNull()
-                    } else return emptyList()
-                    el.add(operator)
+                        operator = "-"; tokens.removeLastOrNull()
+                    } else throw IllegalArgumentException()
+                    tokens.add(operator)
                 }
 
                 c == '*' || c == '/' || c == '^' -> {
-                    if (operator.isNotEmpty()) return emptyList()
+                    if (operator.isNotEmpty()) throw IllegalArgumentException()
                     clearAll()
-                    operator = c.toString(); el.add(operator)
+                    operator = c.toString(); tokens.add(operator)
                 }
 
                 c.isLetterOrDigit() -> {
                     if (operator.isEmpty() && space) {
-                        return emptyList()
+                        throw IllegalArgumentException()
                     } else {
                         operator = ""; space = false
                     }
-                    if (variable.isNotEmpty()) el.removeLast()
-                    variable = "$variable$c"
-                    el.add(variable)
+                    if (variable.isNotEmpty()) tokens.removeLast()
+                    variable += c
+                    tokens.add(variable)
                 }
 
                 c == ' ' -> {
@@ -134,12 +136,13 @@ class Calc {
                 }
 
                 else -> {
-                    return emptyList()
+                    throw IllegalArgumentException()
                 }
             }
         }
-        if (parenthesesCount != 0 || (variable.isEmpty() && el.last() != ")")) el.clear()
-        return el
+        if (parenthesesCount != 0 || (variable.isEmpty() && tokens.last() != ")"))
+            throw IllegalArgumentException()
+        return tokens
     }
 
     fun infixToPostfix(equation: List<String>): MutableList<String> {
@@ -147,30 +150,30 @@ class Calc {
         val operators = mutableListOf<String>()
 
         for (s in equation) {
-            if (s in operatorsMap.keys) {
-                if (operators.isEmpty() || operators.last() == "(") {
-                    operators.add(s)
-                } else if (operators.last() != "(" && operatorsMap[s]!! > operatorsMap[operators.last()]!!) {
-                    operators.add(s)
-                } else if (operators.last() != "(" && operatorsMap[s]!! <= operatorsMap[operators.last()]!!) {
+            when (s) {
+                in operatorsMap.keys -> {
                     while (operators.isNotEmpty()
-                        && operators.last() != "("
-                        && operatorsMap[operators.last()]!! >= operatorsMap[s]!!
+                        && operatorsMap.getOrDefault(operators.last(), 0) >= operatorsMap[s]!!
                     ) {
                         postfix.add(operators.removeLast())
                     }
                     operators.add(s)
                 }
-            } else if (s == "(") {
-                operators.add(s)
-            } else if (s == ")") {
-                var o = operators.removeLast()
-                while (o != "(") {
-                    postfix.add(o)
-                    o = operators.removeLast()
+
+                "(" -> {
+                    operators.add(s)
                 }
-            } else {
-                postfix.add(s)
+
+                ")" -> {
+                    while (operators.last() != "(") {
+                        postfix.add(operators.removeLast())
+                    }
+                    operators.removeLast()
+                }
+
+                else -> {
+                    postfix.add(s)
+                }
             }
         }
         while (operators.isNotEmpty()) {
